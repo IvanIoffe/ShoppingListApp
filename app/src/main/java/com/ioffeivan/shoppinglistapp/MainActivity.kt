3aae95ccb201ed0256331368f21e41e603ef97c5
Ivan Ioffe <iiooffee0202@gmail.com>
2025-08-04 12:48:23 +0300

    feat(app): setup global DI

--- app/src/main/java/com/ioffeivan/shoppinglistapp/MainActivity.kt
+++ app/src/main/java/com/ioffeivan/shoppinglistapp/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.ioffeivan.core.designsystem.theme.ShoppingListAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
