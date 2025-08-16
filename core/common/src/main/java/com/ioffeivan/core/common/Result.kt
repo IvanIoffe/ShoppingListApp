0a371b9b10a051ef80c98c222b9bb4b2f625c0b4
Ivan Ioffe <iiooffee0202@gmail.com>
2025-08-16 11:01:06 +0300

    feat(common): introduce toResultFlow() extension to map Flow<T> -> Flow<Result<T>>

--- core/common/src/main/java/com/ioffeivan/core/common/Result.kt
+++ core/common/src/main/java/com/ioffeivan/core/common/Result.kt
@@ -1,7 +1,16 @@
 package com.ioffeivan.core.common
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+
 sealed class Result<out T> {
     data object Loading : Result<Nothing>()
     data class Success<out T>(val data: T) : Result<T>()
     data class Error(val message: String) : Result<Nothing>()
 }
+
+fun <T> Flow<T>.toResultFlow(): Flow<Result<T>> = map<T, Result<T>> { Result.Success(it) }
+    .onStart { emit(Result.Loading) }
+    .catch { emit(Result.Error(it.message.orEmpty())) }
