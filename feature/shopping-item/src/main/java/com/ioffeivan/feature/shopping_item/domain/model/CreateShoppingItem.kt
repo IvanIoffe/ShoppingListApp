8131098fef15419de93ffa14faf2814fbd34400b
Ivan Ioffe <iiooffee0202@gmail.com>
2025-08-30 21:50:20 +0300

    feat(shopping-item-domain): add models: CreateShoppingItem, DeleteShoppingItem, ShoppingItem, ShoppingItems

---
+++ feature/shopping-item/src/main/java/com/ioffeivan/feature/shopping_item/domain/model/CreateShoppingItem.kt
@@ -0,0 +1,7 @@
+package com.ioffeivan.feature.shopping_item.domain.model
+
+data class CreateShoppingItem(
+    val listId: Int,
+    val name: String,
+    val quantity: String,
+)
---
+++ feature/shopping-item/src/main/java/com/ioffeivan/feature/shopping_item/domain/model/ShoppingItem.kt
@@ -0,0 +1,7 @@
+package com.ioffeivan.feature.shopping_item.domain.model
+
+data class ShoppingItem(
+    val id: Int,
+    val name: String,
+    val quantity: String,
+)
