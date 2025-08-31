4230d96aa4237d632d1f49b51482d1fb93075a16
Ivan Ioffe <iiooffee0202@gmail.com>
2025-08-31 13:54:24 +0300

    feat(shopping-item-data): add ShoppingItemApiService

---
+++ feature/shopping-item/src/main/java/com/ioffeivan/feature/shopping_item/data/source/remote/di/ShoppingItemRemoteSourceModule.kt
@@ -0,0 +1,22 @@
+package com.ioffeivan.feature.shopping_item.data.source.remote.di
+
+import com.ioffeivan.core.network.di.Authorized
+import com.ioffeivan.feature.shopping_item.data.source.remote.ShoppingItemApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.create
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class ShoppingItemRemoteSourceModuleProvider {
+
+    @Singleton
+    @Provides
+    fun provideShoppingItemApiService(@Authorized retrofit: Retrofit): ShoppingItemApiService {
+        return retrofit.create()
+    }
+}
