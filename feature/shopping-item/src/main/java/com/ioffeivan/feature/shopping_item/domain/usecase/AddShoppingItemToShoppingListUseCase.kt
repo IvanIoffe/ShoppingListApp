3073655cc3e5b0250579aa043f1eeabbde36ea08
Ivan Ioffe <iiooffee0202@gmail.com>
2025-09-30 18:13:05 +0300

    refactor(shopping-item): refactor AddShoppingItemToShoppingListUseCase, DeleteShoppingItemFromShoppingListUseCase

--- feature/shopping-item/src/main/java/com/ioffeivan/feature/shopping_item/domain/usecase/AddShoppingItemToShoppingListUseCase.kt
+++ feature/shopping-item/src/main/java/com/ioffeivan/feature/shopping_item/domain/usecase/AddShoppingItemToShoppingListUseCase.kt
@@ -1,7 +1,7 @@
 package com.ioffeivan.feature.shopping_item.domain.usecase
 
 import com.ioffeivan.core.common.Result
-import com.ioffeivan.feature.shopping_item.domain.model.AddShoppingItem
+import com.ioffeivan.feature.shopping_item.domain.model.ShoppingItem
 import com.ioffeivan.feature.shopping_item.domain.repository.ShoppingItemRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -9,7 +9,7 @@ import javax.inject.Inject
 class AddShoppingItemToShoppingListUseCase @Inject constructor(
     private val shoppingItemRepository: ShoppingItemRepository,
 ) {
-    operator fun invoke(addShoppingItem: AddShoppingItem): Flow<Result<Unit>> {
-        return shoppingItemRepository.addShoppingItem(addShoppingItem)
+    operator fun invoke(shoppingItem: ShoppingItem): Flow<Result<Unit>> {
+        return shoppingItemRepository.addShoppingItem(shoppingItem)
     }
 }
--- feature/shopping-item/src/main/java/com/ioffeivan/feature/shopping_item/domain/usecase/DeleteShoppingItemFromShoppingListUseCase.kt
+++ feature/shopping-item/src/main/java/com/ioffeivan/feature/shopping_item/domain/usecase/DeleteShoppingItemFromShoppingListUseCase.kt
@@ -1,13 +1,12 @@
 package com.ioffeivan.feature.shopping_item.domain.usecase
 
-import com.ioffeivan.feature.shopping_item.domain.model.DeleteShoppingItem
 import com.ioffeivan.feature.shopping_item.domain.repository.ShoppingItemRepository
 import javax.inject.Inject
 
 class DeleteShoppingItemFromShoppingListUseCase @Inject constructor(
     private val shoppingItemRepository: ShoppingItemRepository,
 ) {
-    suspend operator fun invoke(deleteShoppingItem: DeleteShoppingItem) {
-        shoppingItemRepository.deleteShoppingItem(deleteShoppingItem)
+    suspend operator fun invoke(id: Int) {
+        shoppingItemRepository.deleteShoppingItem(id)
     }
 }
